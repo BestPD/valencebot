77465de2616fd392fbfadf6cb78e1c8efd8a3ac4
BestPD <41842397+BestPD@users.noreply.github.com>
2018-08-25 16:24:47 -0400

    Update helper.js

--- commands/helper.js
+++ commands/helper.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
   let serverembed = new Discord.RichEmbed()
+  .setDescription("Helper")
   .setColor("#c14545")
   .addField("Helper", "Becoming staff on Valence is not easy work, which is why we search for the highest quality applicants to join our team. As Helper, you are responsible for moderating In Game and on our Discord server, and assisting players accordingly. The requirements for Helper consist of you being active, capable of moderating and punishing rulebreakers, and being mature and helpful. ")
 
