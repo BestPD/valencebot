5d396a8128c36ba758453499c670f45403e9f08c
BestPD <41842397+BestPD@users.noreply.github.com>
2018-08-25 13:09:50 -0400

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
   .setDescription("Welcome to Valence, the best assistant that offers various commands!")
   .setColor("#c14545")
   .addField("What is this bot?", "The Valence Bot is a bot that provides various information about the server, as well as some moderation commands.")
-  .addField("How can I play Valence", "Valence is still in development and this will be updated accordingly.")
+  .addField("How can I play Valence?", "Valence is still in development and this will be updated accordingly.")
   .addField("How can I become a staff member?", "Do !helper for more information.")
   .addField("_ _","Bot created by Tacking for use by Valence")
 
