523b6ae65e769029941948a16f0cb63c39e7d356
BestPD <41842397+BestPD@users.noreply.github.com>
2018-08-25 20:47:21 -0400

    Add files via upload

---
+++ commands/lock.js
@@ -0,0 +1,48 @@
+const ms = require('ms');
+exports.run = (client, message, args) => {
+  if (!client.lockit) client.lockit = [];
+  let time = args.join(' ');
+  let validUnlocks = ['release', 'unlock'];
+  if (!time) return message.reply('You need to set a time for the lockdown.');
+
+  if (validUnlocks.includes(time)) {
+    message.channel.overwritePermissions(message.guild.id, {
+      SEND_MESSAGES: null
+    }).then(() => {
+      message.channel.send('Channel unlocked!');
+      clearTimeout(client.lockit[message.channel.id]);
+      delete client.lockit[message.channel.id];
+    }).catch(error => {
+      console.log(error);
+    });
+  } else {
+    message.channel.overwritePermissions(message.guild.id, {
+      SEND_MESSAGES: false
+    }).then(() => {
+      message.channel.send(`Channel locked down for ${ms(ms(time), { long:true })}`).then(() => {
+
+        client.lockit[message.channel.id] = setTimeout(() => {
+          message.channel.overwritePermissions(message.guild.id, {
+            SEND_MESSAGES: null
+          }).then(message.channel.send('Channel unlocked!')).catch(console.error);
+          delete client.lockit[message.channel.id];
+        }, ms(time));
+
+      }).catch(error => {
+        console.log(error);
+      });
+    });
+  }
+};
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['ld'],
+  permLevel: 3
+};
+
+exports.help = {
+  name: 'lockdown',
+  description: 'This will lock a channel down for the set duration, be it in hours, minutes or seconds.',
+  usage: 'lockdown <duration>'
+};
